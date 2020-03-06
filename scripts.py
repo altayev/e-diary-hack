b19cff3c174f723cfdb4a6c5aa616f283ed38e2c
Sultanbek Altayev <sultanbekaltayev8@gmail.com>
2020-03-06 12:30:39 +0000

    Отформатирован код и readme

--- scripts.py
+++ scripts.py
@@ -8,10 +8,8 @@ def get_schoolkid(name):
         schoolkid = Schoolkid.objects.get(full_name__contains=name)
     except MultipleObjectsReturned:
         print(f"Найдено несколько учеников с именем {name}.")
-        return None
     except DoesNotExist:
         print(f"Ученика по имени {name} не существует.")
-        return None
     return schoolkid
 
 
@@ -31,6 +29,17 @@ def fix_marks(name):
 
 def create_commendation(name, subject_title):
     schoolkid = get_schoolkid(name)
-    commendations = ["Молодец!", "Хвалю!", "Отличная работа!", "Супер!", "У тебя талант!", "Ты сегодня прыгнул выше головы!", "Это как раз то, что нужно!", "Я тобой горжусь!"]
-    last_lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter, subject__title=subject_title).order_by('-date')[0]
-    Commendation.objects.create(text=random.choice(commendations), created=last_lesson.date, schoolkid=schoolkid, subject=last_lesson.subject, teacher=last_lesson.teacher)
+    commendations = [
+        "Молодец!",
+        "Хвалю!",
+        "Отличная работа!",
+        "Супер!",
+        "У тебя талант!",
+        "Ты сегодня прыгнул выше головы!",
+        "Это как раз то, что нужно!",
+        "Я тобой горжусь!",
+    ]
+    last_lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter, subject__title=subject_title).order_by('-date')[0]
+    Commendation.objects.create(text=random.choice(commendations), created=last_lesson.date,
+        schoolkid=schoolkid, subject=last_lesson.subject, teacher=last_lesson.teacher)
